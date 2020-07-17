aa5022c34a8da41e6ce5f0c0f8c97b4bef94983d
ThienejLee <thienvanlea1@gmail.com>
2020-07-18 00:03:42 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/list_with_image_and_text/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_with_image_and_text
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,40 @@
+import 'flower.dart';
+
+var flowers = [
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+  new Flower(
+    flowerName: 'Chi Hoa Hồng',
+    description: 'Hồng hay hường là tên gọi chung cho các loài thực vật có hoa dạng cây bụi hoặc cây leo lâu năm.',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rosa_chinensis_petals.jpg/560px-Rosa_chinensis_petals.jpg',
+  ),
+
+];
